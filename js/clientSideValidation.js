664be7c11fd1ec2c43fba2ceb2703ff78183722a
SJarvis <simon.jarvis@positivepurchasing.com>
2019-08-07 14:50:29 +0100

    tweaks again

--- js/clientSideValidation.js
+++ js/clientSideValidation.js
@@ -2,34 +2,41 @@ var validate = {};
 
 (function (self) {
     let stepValid = true;
-
     self.init = function () {
         $('input').each(function () {
             let type = $(this).attr('type');
-            //console.log(type, $(this))
 
-            switch (type) {
-                case 'radio':
-                case 'checkbox':
+            if ($(this).attr('data-checkbox-group')) {
+                let groupName = $(this).attr('data-checkbox-group');
+                if ($(this).attr('data-checkbox-required')) {
+                    
                     $(this).on('click', function () {
-                        self.input($(this))
+                        self.validateInputGroup($('[name='+groupName+']'));
                     });
-                    break;
-                default:
-                    $(this).on('blur', function () {
-                        self.input($(this))
-                    });
-                    $(this).on('input', function () {
-                        self.input($(this))
-                    });
-                    $(this).on('change', function () {
-                        self.input($(this))
-                    });
-                    break;
+                }
+            } else {
+                switch (type) {
+                    case 'radio':
+                    case 'checkbox':
+                        $(this).on('click', function () {
+                            self.input($(this))
+                        });
+                        break;
+                    default:
+                        $(this).on('blur', function () {
+                            self.input($(this))
+                        });
+                        $(this).on('input', function () {
+                            self.input($(this))
+                        });
+                        $(this).on('change', function () {
+                            self.input($(this))
+                        });
+                        break;
+                }
             }
-
         });
-
+        //console.log(checked);
         $('textarea').each(function () {
             $(this).on('blur', function () {
                 self.input($(this))
@@ -48,31 +55,61 @@ var validate = {};
             });
         });
     }
-    
+
+    self.validateInputGroup = function (group) {
+        console.log('valinputgroup')
+        let checked = [],
+            amountRequired,
+            target = group.attr('data-validation-target');
+        group.each(function () {
+            if (this.checked) {
+                checked.push(this.checked);
+                amountRequired = group.attr('data-checkbox-required');
+            }
+        });
+        console.log('budgie', group, checked, amountRequired)
+        if (checked.length >= amountRequired) {
+            self.valid(target);
+        } else {
+            console.log(this, $(this))
+            self.invalid($(this), target)
+        }
+                
+    }
+
     self.input = function (el)  {
         let target = el.attr('data-validation-target');
         
         //If valid
         if (el[0].checkValidity()) {
-            $('[data-validation-output=' + target + ']').removeClass('invalid');
-            $('#' + target + '-message').remove();
+            self.valid(target)
         } else {
-            // Else if invalid
-            let container = $('[data-validation-output=' + target + ']');
-            container.addClass('invalid');
-            stepValid = false;
-            if (container[0]) {
-                // Dynamic
-                let textCont = document.createElement('span');
-                textCont.setAttribute('id', target + '-message');
-                textCont.setAttribute('class', 'validation-message validation-message--invalid');
-                let message = el.attr('data-validation-message') === undefined ? 'Input invalid, please complete correctly' : el.attr('data-validation-message');
-                let text = document.createTextNode(message);
+             // Else if invalid
+            self.invalid(el, target)
+        }
+    }
 
-                textCont.appendChild(text);
-                if ($('[data-validation-output=' + target + ']' + ' .validation-message')[0] !== undefined) return;
-                container[0].appendChild(textCont);
-            }
+    self.valid = function (target) {
+        $('[data-validation-output=' + target + ']').removeClass('invalid');
+        $('#' + target + '-message').remove();
+    }
+
+    self.invalid = function (el, target) {
+        console.log(el, target)
+        let container = $('[data-validation-output=' + target + ']');
+        container.addClass('invalid');
+        stepValid = false;
+        if (container[0]) {
+            // Dynamic
+            let textCont = document.createElement('span');
+            textCont.setAttribute('id', target + '-message');
+            textCont.setAttribute('class', 'validation-message validation-message--invalid');
+            let message = el.attr('data-validation-message') === undefined ? 'Input invalid, please complete correctly' : el.attr('data-validation-message');
+            let text = document.createTextNode(message);
+
+            textCont.appendChild(text);
+            if ($('[data-validation-output=' + target + ']' + ' .validation-message')[0] !== undefined) return;
+            container[0].appendChild(textCont);
         }
     }
 
@@ -80,7 +117,15 @@ var validate = {};
         stepValid = true;
         let inputs = [];
         $('.step-active input').each(function () {
-            inputs.push($(this));
+            if ($(this).attr('data-checkbox-group')) {
+                let groupName = $(this).attr('data-checkbox-group');
+                if ($(this).attr('data-checkbox-required')) {
+
+                    $(this).on('click', function () {
+                        self.validateInputGroup($('[name=' + groupName + ']'));
+                    });
+                }
+            } else inputs.push($(this));
         })
 
         $('.step-active textarea').each(function () {
@@ -101,9 +146,14 @@ var validate = {};
     self.form = function () {
         let inputs = [];
         $('form input').each(function () {
-            inputs.push($(this));
-        })
+            if ($(this).attr('data-checkbox-group')) {
+                let groupName = $(this).attr('data-checkbox-group');
 
+                if ($(this).attr('data-checkbox-required')) {
+                    self.validateInputGroup($('[name=' + groupName + ']'));
+                }
+            } else inputs.push($(this));
+        })
         $('form textarea').each(function () {
             inputs.push($(this));
         })
